c578eadbbc7e8a300848ba4c2a5f8c9fe22d1e0d
Josie Sousa <129207028+Joyzy79@users.noreply.github.com>
2023-07-08 00:27:21 +0100

    Add files via upload

---
+++ sevenDays-of-code2/script.js
@@ -0,0 +1,17 @@
+/*- Qual o seu nome?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando?
+Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.
+1 > Muito bom! Continue estudando e você terá muito sucesso.
+2 > Ahh que pena... Já tentou aprender outras linguagens?
+if (resposta == 1){
+    // dê a resposta positiva
+}
+if (resposta == 2){
+    // dê a resposta negativa
+}
+const cidade = prompt("Digite a sua cidade:");
+const msg = `Você é de ${cidade}!`;
+alert(msg);*/
+
+
